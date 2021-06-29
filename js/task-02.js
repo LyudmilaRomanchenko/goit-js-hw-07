234582e07fb15fa520acc051838dd4c9be738523
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-06-30 00:55:23 +0300

    Добавляет 3 задачи

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientsCallBack = item => {
+  const elem = document.createElement('li');
+  elem.textContent = item;
+  return elem;
+};
+
+// ========================
+// const createIngredients = ingredients.reduce((acc, ingredient) =>
+//   acc + ingredientsCallBack(ingredient), "");
+
+// console.log(createIngredients);
+
+// ============================
+const createIngredients = ingredients.map(ingredient =>
+  ingredientsCallBack(ingredient));
+
+
+console.log(createIngredients);
+
+
+ingredientsList.append(...createIngredients);
+
+
+ 
