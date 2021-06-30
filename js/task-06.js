bde9070dadf392da94841f1eb48a4ed6c5e65f16
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-06-30 22:57:03 +0300

    Задаа 6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const getInput = document.querySelector('#validation-input');
+
+getInput.addEventListener('input', onFormInput);
+
+function onFormInput()
+
+console.log(getInput);
+//if ()
+//const a = getInput.formData
+const formData = new FormData();
+
+  console.log(formData);
