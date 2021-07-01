947875f54bab6fedceb7c87a681193acd2237d48
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-07-01 21:50:54 +0300

    Добавляет задачу 5, 7

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputElem = document.querySelector('#name-input');
+//console.log(inputElem);
+
+const nameElem = document.querySelector('#name-output');
+//console.log(nameElem);
+
+
+const onInputName = (event) => {
+    const userName = event.currentTarget.value;
+    console.log(userName);
+    return nameElem.textContent = `${userName}`
+}
+
+inputElem.addEventListener('input', onInputName);
