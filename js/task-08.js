a2107a7f2810e6e411acec4d971f9a65f2a1bb39
LyudmilaRomanchenko <nowbeauty31@gmail.com>
2021-07-05 22:27:01 +0300

    Исправляет задачу 8

--- js/task-08.js
+++ js/task-08.js
@@ -7,6 +7,10 @@ console.log(destroyElem);
 const boxesElem = document.querySelector('#boxes');
 console.log(boxesElem);
 
+let boxWidth = 30;
+let boxHight = 30;
+
+
 const createBoxes = (amount) => {
     const arrayElem = [];
     for (let i = 0; i < amount * 10; i += 10) {
@@ -20,9 +24,14 @@ const createBoxes = (amount) => {
             col=r+g+d;
             return col;
         }
-       let elem = document.createElement('div');
-        elem.style.width = `${30+i}px`;
-        elem.style.height = `${30+i}px`;
+
+        let elem = document.createElement('div');
+        elem.style.width = `${boxWidth + i}px`;
+        elem.style.height = `${boxHight + i}px`;
+        
+        boxWidth = boxWidth + i;
+        boxHight = boxHight + i;
+
         elem.style.backgroundColor = `#${randomColor()}`;
         elem.style.borderRadius = '10px';
         elem.style.margin = '5px';
@@ -32,6 +41,13 @@ const createBoxes = (amount) => {
         arrayElem.push(elem);
         //boxesElem.appendChild(elem);
     }
+    console.log(boxWidth);
+    console.log(boxHight);
+
+
+    boxWidth = boxWidth + 10;
+    boxHight = boxHight + 10;
+
     console.log(arrayElem);
     boxesElem.append(...arrayElem);
     //return console.log(boxesElem);
@@ -48,6 +64,8 @@ renderElem.addEventListener('click', onCreateElem);
 
 const onDestroyElem = () => {
     boxesElem.textContent = '';
+    boxWidth = 30;
+    boxHight = 30
     //console.log('Очистить');
 }
 
